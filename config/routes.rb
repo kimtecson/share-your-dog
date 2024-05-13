b993af16ae562c75336efb17bbeca72bb53c5a0a
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-13 07:51:12 +0200

    added correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'dogs/index'
-  get 'dogs/show'
-  get 'dogs/create'
-  get 'dogs/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/bookings'
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/', to: 'pages#home' as :home
+  resources :dogs, only: [:show, :index, :create, :new] do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:index]
 end
