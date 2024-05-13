87f6f1ec504eb292fa7b85e9656caf704d6510d0
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-13 08:01:55 +0200

    added namespace in routes and new owner controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'pages#home' as :home
+  get '/', to: 'pages#home', as: :home
   resources :dogs, only: [:show, :index, :create, :new] do
     resources :bookings, only: [:create]
   end
   resources :bookings, only: [:index]
+
+  namespace :owner do
+    resources :bookings, only: :index
+  end
 end
