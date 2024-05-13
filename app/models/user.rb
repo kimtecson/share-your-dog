766ad856ede6745da03affbc6e08f8f908e00f32
Matthew Davies <m.davies.work@gmail.com>
2024-05-13 18:20:17 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :dogs, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo
+  # has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true, length: { within: 3..20 }
