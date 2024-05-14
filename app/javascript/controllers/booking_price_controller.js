d3ce3ba6cd959f273b26dc0d1cad3672b1f58bab
kimberly tecson <tecsonkim@gmail.com>
2024-05-15 08:59:59 +0900

    added the show pages price

---
+++ app/javascript/controllers/booking_price_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="booking-price"
+export default class extends Controller {
+  static targets = ["start_date", "end_date", "price", "info"];
+  static values = { price: Number };
+
+  update() {
+    const pricePerDay = parseInt(this.priceValue, 10);
+    // make sure the user has chosen a start date and end date
+    if (
+      this.start_dateTarget.value !== "" &&
+      this.end_dateTarget.value !== ""
+    ) {
+      // calculating the time from the values in the HTML
+      const diffInMs =
+        new Date(this.end_dateTarget.value) -
+        new Date(this.start_dateTarget.value);
+      const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+      if (diffInDays > 0) {
+        // displays the total price per nigh
+        this.infoTarget.innerText = `¥${pricePerDay} x ${diffInDays * 24} hours`;
+        this.priceTarget.innerHTML = `<span>¥${(
+          diffInDays * pricePerDay *24
+        ).toLocaleString()}</span> <small class='fw-light'>total</small> `;
+      } else if (diffInDays === 0) {
+        // displays 1-night minimum if end date is same as start date
+        this.infoTarget.innerText = "";
+        this.priceTarget.innerHTML =
+          "<span class='text-danger fw-light'>1-night mininum</span>";
+      } else {
+        // displays invalid if the end date is before the start or not overnight
+        this.infoTarget.innerText = "";
+        this.priceTarget.innerHTML =
+          "<span class='text-danger fw-light'>Invalid dates</span>";
+      }
+    }
+  }
+}
