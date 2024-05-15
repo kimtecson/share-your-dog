82991f5faa194f4153c151a8f2d02234db0fddc2
Matthew Davies <m.davies.work@gmail.com>
2024-05-15 14:01:11 +0900

    setup

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -27,6 +27,12 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
   private
 
   def booking_params
