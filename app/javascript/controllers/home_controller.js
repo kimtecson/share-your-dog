997e91cdcf9e70fa11f281743897e4fca7ed870f
stanley zalewski <mrstanleyzalewski@gmail.com>
2024-05-17 04:37:52 +0200

    card update

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+  static targets = ['dog-1', 'dog-2', 'dog-3', 'meet-dog']
+
   connect() {
     console.log("Hello from home_controller.js");
+  }
+
+  displayDog() {
+    console.log('hello world!')
   }
 }
